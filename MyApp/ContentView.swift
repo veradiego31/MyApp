07faf7a0bcd761aaa0fa317386b42b3876ddcd1e
veradiego31 <ingvera31@gmail.com>
2023-09-26 20:13:32 -0500

    Initial Commit

---
+++ MyApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyApp
+//
+//  Created by Diego Vera on 2023-09-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
