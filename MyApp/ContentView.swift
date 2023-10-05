dbaa2af93a24f2c7f978fcf622b8d86c653da912
veradiego31 <veradiego31@users.noreply.github.com>
2023-10-05 18:31:18 -0500

    add new stack and text

--- MyApp/ContentView.swift
+++ MyApp/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, App!")
+            HStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, App!")
+            }
+            Text("this is description")
+                .bold()
         }
         .padding()
     }
